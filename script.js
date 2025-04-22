78f2a827b9fdf83e3544e77626d3b6858feb808a
Henrique de Oliveira <henrique2801oliveira@gmail.com>
2025-04-22 20:57:35 -0300

    Adicionando o verificador de idade na compra de bebidas

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ modificar.addEventListener('click', function darSpoiler(){
     const fisrtTxt = document.getElementById('textoInicial');
     fisrtTxt.textContent = "Personagem Jiraya morrer para o Pain kkk";
 })
+
+
+
+const validar = document.getElementById('validarIdade');
+validar.addEventListener('click', function validarIdade(){
+    const idade = document.getElementById('idadeUsu');
+    const resposta = document.getElementById('respostaIdade');
+    if (!isNaN(idade)){
+        resposta.textContent = "Você nem digitou um número válido";        
+    }else if(idade < 18){
+        resposta.textContent = "Você não tem idade para o que está fazendo agora";
+    }else{
+        resposta.textContent = "Compra autorizada"
+    }
+})
+
